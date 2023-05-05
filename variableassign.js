6d0a5ce28873cf751b35b8ddb3fbd3da7506c754
nikii6838 <nikii6838@gmail.com>
2023-05-05 16:06:31 +0530

    assignment

--- variableassign.js
+++ variableassign.js
@@ -1,17 +1,34 @@
+var firstName =
+    console.log(firstName);
+
+
+var firstName = "Nikita Mane";
+console.log(firstName);
+
+
+var firstName = "Nikita Pravin Mane";
+console.log(firstName);
+
+var myScript = "I love Only JS";
+console.log(myScript);
+
 var firstName = "nikita";
 console.log(firstName);
 
 var LastName = "Mane";
 console.log(LastName);
 
-var age="22";
-console.log("age");
+var age = 22;
+console.log(age);
 
-var address= "shahu colony,karve nagar"
-console.log("address")
+var address = "shahu colony,karve nagar"
+console.log(address);
 
-var pincode="411052"
-console.log("pincode")
+var pincode = "411052"
+console.log(pincode);
 
-var gender ="Female"
-console.log("gender")
+var gender = "Female"
+console.log(gender);
+
+var isMarried = "No"
+console.log(isMarried);
