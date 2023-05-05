c4a31fcf093b0041b5dc6001119e0bc0f0ba79f4
nikii6838 <nikii6838@gmail.com>
2023-05-05 15:05:25 +0530

    assignment

---
+++ hello.js
@@ -0,0 +1,16 @@
+console.log("nikita mane..");
+
+var firstName = "nikita";
+console.log(firstName);
+
+var age; // Variable Declaration 
+age = "22";  // Variable Initialization
+console.log(age);
+
+var holder; // Variable Declaration
+console.log(holder); // undefined
+holder = "My Spec"; // Variable Initialization
+console.log(holder);
+
+holder = "Sachin Spec"; // Variable update
+console.log(holder); //
---
+++ variableassign.js
@@ -0,0 +1,17 @@
+var firstName = "nikita";
+console.log(firstName);
+
+var LastName = "Mane";
+console.log(LastName);
+
+var age="22";
+console.log("age");
+
+var address= "shahu colony,karve nagar"
+console.log("address")
+
+var pincode="411052"
+console.log("pincode")
+
+var gender ="Female"
+console.log("gender")
