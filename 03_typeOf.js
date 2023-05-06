e430e43e824fdb67b3277556e89c5f5a51d93b5c
nikii6838 <nikii6838@gmail.com>
2023-05-06 16:19:54 +0530

    assignment

---
+++ 03_typeOf.js
@@ -0,0 +1,31 @@
+var bankname;
+console.log("value and type of bank name before assign:",bankname,typeof bankname) ;
+bankname= 'SBI bank';
+console.log("value and type of bank name after intialization:",bankname, typeof bankname);
+console.log("\n");
+var marks='90%';
+console.log("value and types of marks",marks,typeof marks);
+console.log("\n");
+var isWorking;
+console.log("value and type of bank name before intialization:",isWorking,typeof isWorking);
+isWorking=true;
+console.log("value and type of bank name after intialization:",isWorking,typeof isWorking);
+console.log("\n");
+var totalcount ="hundred and seven";
+console.log("value and Data type of totalcount :",totalcount,typeof totalcount);
+totalcount=107;
+console.log("value and Data type of totalcount:" , totalcount ,typeof totalcount);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
