f2009c22449a7e65a6654427b9e110dff0b71972
nikii6838 <nikii6838@gmail.com>
2023-06-08 23:59:57 +0530

    practice

--- practice.js
+++ practice.js
@@ -1,31 +1,17 @@
-console.log(`....05. Operator Assignment .......`);
-console.log(`1. Normal Function for "javascript" "Google chrome" "developer smart".`);
-function squareOfWordLength(str) {
-var lengthOFWord = str.length;
-console.log(`Length of word is : ${lengthOFWord}`);
-var squareOfword = lengthOFWord * lengthOFWord;
-console.log(`String Length square is : ${squareOfword}`);
+function maleMarriageEligibility(gender, age, boyName) {
+  const msg = gender === "Male" && age >= 21 ? `Hey ${boyName}, you are eligible for marriage` : "Not eligible for marriage";
+  return msg;
 }
-squareOfWordLength("JavaScript");
-squareOfWordLength("Google Chrome");
-squareOfWordLength("Developer Smart");
-console.log(`....................................`);
 
-function reactDev() {
-    var stringAsgiven = "I am Angular Developer";
-    var strlength = stringAsgiven.length;
-    console.log(`Length of Given string is : ${strlength}`);
-    var splittedAns = stringAsgiven.split(" ");
-    console.log(`Splitted string is : ${splittedAns}`);
+// Test cases for maleMarriageEligibility
+console.log(maleMarriageEligibility("Male", 25, "Bill Gates"));
+console.log(maleMarriageEligibility("Male", 17, "Steve Jobs"));
 
-    var splittedAnslength = splittedAns.length;
-    console.log(`Total no of words available in String is : ${splittedAnslength}`);
-    var divideResult = strlength / splittedAnslength;
-    
-    console.log(`---Division---`);
-    console.log(`String length / divided by total no of words : ${divideResult}`);
-    var multiplyResult = strlength * splittedAnslength;
-    console.log(`---Multiplication---`);
-    console.log(`String length * total no of words : ${multiplyResult}`);
+function femaleMarriageEligibility(gender, age, girlName) {
+  const msg = gender === "Female" && age >= 18 ? `Hey ${girlName}, you are eligible for marriage` : "Not eligible for marriage";
+  return msg;
 }
-reactDev();
+
+// Test cases for femaleMarriageEligibility
+console.log(femaleMarriageEligibility("Female", 16, "Jennifer"));
+console.log(femaleMarriageEligibility("Female", 27, "Melinda Gates"));
