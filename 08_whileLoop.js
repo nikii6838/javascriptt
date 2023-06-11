853dad8125b94c917ab8b796144fbbf1eb4c0571
nikii6838 <nikii6838@gmail.com>
2023-06-11 20:17:33 +0530

    mongo db and sql assignment

--- 08_whileLoop.js
+++ 08_whileLoop.js
@@ -1,24 +1,23 @@
-// WAP to print number from 1 to 10
-let i=0; // initialization i = 1   2  3
-while (i<=10) { // condition 0<=10
-   console.log(i); // 0  1 2
-   i = i + 1;
-}
+// 11
+// 2
 
-// WAP to print even numbers till 30
-console.log("WAP to print even numbers till 30");
-
-let j=0;  // initialization
-while (j<=30) {  // condition
-   if (j%2==0) {
-      console.log(j);
+function primeNum(num) {
+   let isPrime = true;
+   for (let index = 2; index < num; index++) {
+     if (num % index == 0) {
+       isPrime = false;
+       console.log(`Not a prime number as it is divisible by ${index}`);
+       break;
+     }
    }
-   j++; // update expression
-}
-
-// WAP to print number from 100 to 90
-let k = 100; 
-while (k>=90) {
-   console.log(k);
-   k--;
-}
+   if (isPrime) {
+     console.log(`${num} is prime number`);
+   } else {
+     console.log(`Is not prime number ${num}`);
+   }
+ }
+ 
+ primeNum(4);
+ primeNum(5);
+ primeNum(9);
+ primeNum(11);
